8fb464702887971676bf1abd7e0a279eda241781
moraleseufracio <1367162@senati.pe>
2022-11-09 10:18:08 -0500

    tercer paso

--- resources/views/turismo.blade.php
+++ resources/views/turismo.blade.php
@@ -2,7 +2,7 @@
 echo " City Tours"
 ?>
 
-<button type="button" class="btn btn-primary btn-sm"> <a href="https://www.google.com.mx/" class="underline text-gray-900 dark:text-white">BUSCAR TU LUGAR TURISTICOS</a></button>
+<button type="button" class="btn btn-primary btn-sm"> <a href="https://www.google.com.mx/" class="underline text-gray-900 dark:text-white">BUSCA TU LUGAR TURISTICOS</a></button>
 <head>
     <style>
         body {
@@ -17,7 +17,7 @@ echo " City Tours"
             display:block;
             margin:50px;
             width:700px;
-            height:auto;
+            height:50%;
 
         }
     </style>
@@ -27,29 +27,45 @@ echo " City Tours"
 <BODY>
 <div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="false">
   <div class="carousel-indicators">
-    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+    <button type="button" data-bs-target="cusco" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+    <button type="button" data-bs-target="lima" data-bs-slide-to="1" aria-label="Slide 2"></button>
     <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
   </div>
-  <div class="sona turistico">
-    <div class="cusco">
-      <img src="https://travel1tours.com/wp-content/uploads/2018/09/cusco-lugares-turisticos.jpg" class="d-block w-100" alt="lugar turistico de cusco">
+  <div class="sona turistico" >
+    <div class="cusco" >
+    <h5>LAS ZONAS TURISTICAS EN CUSCO </h5>
+      <img src="https://travel1tours.com/wp-content/uploads/2018/09/cusco-lugares-turisticos.jpg  "  class="d-block w-100"  alt="lugar turistico de cusco"> 
       <div class="carousel-caption d-none d-md-block">
         <h5>First slide label</h5>
-        <p>Some representative placeholder content for the first slide.</p>
+        <a href="https://www.machu-picchu.org/es/guias-y-consejos/lugares-turisticos-cusco/" class="underline">SIGUIENTE AQUI</a> <br> GRACIAS.
+        
       </div>
     </div>
     <div class="lima">
-      <img src="..." class="d-block w-100" alt="...">
+    <h5>LAS ZONAS TURISTICAS EN LIMA  </h5>
+      <img src="https://travel1tours.com/wp-content/uploads/2020/07/lugares-turisticos-lima.jpg" class="d-block w-100"  alt="lima">
       <div class="carousel-caption d-none d-md-block">
-        <h5>Second slide label</h5>
-        <p>Some representative placeholder content for the second slide.</p>
+        <h5>Lima es la capital de Perú ubicada en la árida costa del Pacífico del país.
+             <br> Pese a que su centro colonial se conserva, es una desbordante metrópolis y una de las ciudades más grandes de Sudamérica. 
+             <br> El Museo Larco alberga una colección de arte precolombino y el Museo de la Nación recorre la historia de las civilizaciones antiguas de Perú.
+              <br> La Plaza de Armas y la catedral del siglo XVI son el núcleo del antiguo centro de Lima.</h5>
+              <a href="https://www.denomades.com/blog/lugares-turisticos-lima-debes-conocer/" class="underline">SIGUIENTE AQUI</a> <br> GRACIAS.
       </div>
     </div>
     <div class="tingo maria">
-      <img src="..." class="d-block w-100" alt="...">
+    <h5>LAS ZONAS  TURISTICA DE TINGO MARIA</h5>
+      <img src="https://www.tingomarianunashtours.com.pe/assets/webimg/2021-10-29_OIOICQIG.jpg" class="d-block w-100" alt="zona turistica de tingo maria">
       <div class="carousel-caption d-none d-md-block">
-        <h5>Third slide label</h5>
+        <h5>AAAA</h5>
+        <p>Some representative placeholder content for the third slide.</p>
+      </div>
+    </div>
+  </div>
+  <div class="tingo maria">
+    <h5>LAS ZONAS  TURISTICA DE ICA</h5>
+      <img src="https://sites.google.com/site/regionicajulia/_/rsrc/1413838763903/home/Imagen1.jpg" class="d-block w-100" alt="zona turistica de ica ">
+      <div class="carousel-caption d-none d-md-block">
+        <h5>AAAA</h5>
         <p>Some representative placeholder content for the third slide.</p>
       </div>
     </div>
@@ -61,6 +77,9 @@ echo " City Tours"
   <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
     <span class="carousel-control-next-icon" aria-hidden="true"></span>
     <span class="visually-hidden">Next</span>
+    
   </button>
+  
+
 </div>
 </BODY>
