11675c132b49ef925aeef4bbb6073b4136625a06
moraleseufracio <1367162@senati.pe>
2022-11-10 11:36:14 -0500

    cuarto paso

--- resources/views/turismo.blade.php
+++ resources/views/turismo.blade.php
@@ -31,42 +31,75 @@ echo " City Tours"
     <button type="button" data-bs-target="lima" data-bs-slide-to="1" aria-label="Slide 2"></button>
     <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
   </div>
-  <div class="sona turistico" >
+  <div class="lugares turísticos de las diferentes regiones del Perú." >
     <div class="cusco" >
     <h5>LAS ZONAS TURISTICAS EN CUSCO </h5>
       <img src="https://travel1tours.com/wp-content/uploads/2018/09/cusco-lugares-turisticos.jpg  "  class="d-block w-100"  alt="lugar turistico de cusco"> 
       <div class="carousel-caption d-none d-md-block">
-        <h5>First slide label</h5>
+        <h5>Cuzco es una ciudad de los Andes peruanos que fue la capital del Imperio Inca y es conocida por sus restos arqueológicos <br>
+         y la arquitectura colonial española. <br>
+         La Plaza de Armas es el centro de la ciudad antigua, con galerías, balcones de madera tallada y ruinas de murallas incas. 
+         <br> El convento de Santo Domingo, de estilo barroco, se construyó sobre el Templo del Sol inca (Qoricancha) y tiene restos arqueológicos de cantería inca.</h5>
         <a href="https://www.machu-picchu.org/es/guias-y-consejos/lugares-turisticos-cusco/" class="underline">SIGUIENTE AQUI</a> <br> GRACIAS.
         
       </div>
     </div>
-    <div class="lima">
+    <div class="lugares turísticos de las diferentes regiones del Perú.">
     <h5>LAS ZONAS TURISTICAS EN LIMA  </h5>
       <img src="https://travel1tours.com/wp-content/uploads/2020/07/lugares-turisticos-lima.jpg" class="d-block w-100"  alt="lima">
       <div class="carousel-caption d-none d-md-block">
         <h5>Lima es la capital de Perú ubicada en la árida costa del Pacífico del país.
              <br> Pese a que su centro colonial se conserva, es una desbordante metrópolis y una de las ciudades más grandes de Sudamérica. 
-             <br> El Museo Larco alberga una colección de arte precolombino y el Museo de la Nación recorre la historia de las civilizaciones antiguas de Perú.
+             <br> El Museo Larco alberga una colección de arte precolombino <br>
+              y el Museo de la Nación recorre la historia de las civilizaciones antiguas de Perú.
               <br> La Plaza de Armas y la catedral del siglo XVI son el núcleo del antiguo centro de Lima.</h5>
               <a href="https://www.denomades.com/blog/lugares-turisticos-lima-debes-conocer/" class="underline">SIGUIENTE AQUI</a> <br> GRACIAS.
       </div>
     </div>
-    <div class="tingo maria">
+    <div class="lugares turísticos de las diferentes regiones del Perú.">
     <h5>LAS ZONAS  TURISTICA DE TINGO MARIA</h5>
       <img src="https://www.tingomarianunashtours.com.pe/assets/webimg/2021-10-29_OIOICQIG.jpg" class="d-block w-100" alt="zona turistica de tingo maria">
       <div class="carousel-caption d-none d-md-block">
-        <h5>AAAA</h5>
-        <p>Some representative placeholder content for the third slide.</p>
+        <h5>Tingo María es una ciudad en el río Huallaga, en el centro de Perú, entre las montañas andinas y la selva amazónica. 
+        <br> Aquí se encuentra el Parque nacional Tingo María que posee una abundante biodiversidad y <br>
+         es conocido por la Bella Durmiente, una montaña con la forma de una mujer acostada. 
+        <br> Además, aquí está la Cueva de las Lechuzas, que alberga guácharos. Algunas de las cascadas locales son Santa Carmen, <br>
+         con una piscina natural, y Gloriapata, <br>
+         a la que se accede a través de un puente suspendido</h5>
+         <a href="https://www.munitingomaria.gob.pe/mplp/content/lugares-turisticos" class="underline">SIGUIENTE AQUI</a> <br> GRACIAS.
       </div>
     </div>
   </div>
-  <div class="tingo maria">
+  <div class="lugares turísticos de las diferentes regiones del Perú.">
     <h5>LAS ZONAS  TURISTICA DE ICA</h5>
       <img src="https://sites.google.com/site/regionicajulia/_/rsrc/1413838763903/home/Imagen1.jpg" class="d-block w-100" alt="zona turistica de ica ">
       <div class="carousel-caption d-none d-md-block">
-        <h5>AAAA</h5>
-        <p>Some representative placeholder content for the third slide.</p>
+        <h5>Ica es una ciudad del centro sur del Perú, capital del departamento de Ica, <br>
+         situada en el estrecho valle que forma el río Ica, entre el Gran Tablazo de Ica y las <br>
+          laderas occidentales de la cordillera de los Andes. <br>
+         La ciudad de Ica incluye cinco distritos urbanos.</h5>
+         <a href="https://www.viajesfantasticos.net/lugares-turisticos-de-ica/" class="underline">SIGUIENTE AQUI</a> <br> GRACIAS.
+      </div>
+    </div>
+  </div>
+  <div class="lugares turísticos de las diferentes regiones del Perú.">
+    <h5>LAS ZONAS  TURISTICA DE PASCO</h5>
+      <img src="https://www.inforegion.pe/wp-content/uploads/2013/07/MAPA-DEL-CORREDOR-TURISTICO-QUEBRADA-CHAUPIHUARANGA.jpg" class="d-block w-100" alt="zona turistica de Pasco ">
+      <div class="carousel-caption d-none d-md-block">
+        <h5>Pasco es un departamento de la República del Perú ubicado en el centro del país, con capital en Cerro de Pasco. <br>
+         Comprende una región andina en su parte oeste y una amazónica en la oriental. Limitando al norte con Huánuco, <br>
+         al este con Ucayali, al sur con Junín y al oeste con Lima.</h5>
+         <a href="https://www.viajesfantasticos.net/lugares-turisticos-de-pasco/" class="underline">SIGUIENTE AQUI</a> <br> GRACIAS.
+      </div>
+    </div>
+  </div>
+  <div class="lugares turísticos de las diferentes regiones del Perú.">
+    <h5>LAS ZONAS  TURISTICA DE HUANUCO</h5>
+      <img src="https://www.somosperu.org.pe/wp-content/uploads/2016/10/Huanuco-turismo.jpg" class="d-block w-100" alt="zona turistica de huánuco ">
+      <div class="carousel-caption d-none d-md-block">
+        <h5>Huánuco es una ciudad peruana, capital del distrito, la provincia <br>
+         y el departamento homónimos en el centro norte del país.</h5>
+         <a href="https://www.viajesfantasticos.net/lugares-turisticos-de-huanuco/" class="underline">SIGUIENTE AQUI</a> <br> GRACIAS.
       </div>
     </div>
   </div>
