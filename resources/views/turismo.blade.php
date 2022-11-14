de118f4a37588568991d9976b39f2e953863dcce
moraleseufracio <1367162@senati.pe>
2022-11-14 09:41:14 -0500

    final del trabajo

--- resources/views/turismo.blade.php
+++ resources/views/turismo.blade.php
@@ -1,5 +1,5 @@
 <?php
-echo " City Tours"
+echo " Lugares turísticos de las diferentes regiones del Perú."
 ?>
 
 <button type="button" class="btn btn-primary btn-sm"> <a href="https://www.google.com.mx/" class="underline text-gray-900 dark:text-white">BUSCA TU LUGAR TURISTICOS</a></button>
@@ -10,6 +10,7 @@ echo " City Tours"
         }
         a{ 
             Color:#f00;
+            font-size:100%
         }
         img{
             border:3px solid #000;
@@ -18,7 +19,19 @@ echo " City Tours"
             margin:50px;
             width:700px;
             height:50%;
+       img2{
 
+            }
+
+          
+        }
+        .turismo{
+          font-size:100%;
+          
+        }
+        .zona{
+          font-size:110%;
+          
         }
     </style>
 </head>
@@ -32,10 +45,10 @@ echo " City Tours"
     <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
   </div>
   <div class="lugares turísticos de las diferentes regiones del Perú." >
-    <div class="cusco" >
+    <div class="zona" >
     <h5>LAS ZONAS TURISTICAS EN CUSCO </h5>
       <img src="https://travel1tours.com/wp-content/uploads/2018/09/cusco-lugares-turisticos.jpg  "  class="d-block w-100"  alt="lugar turistico de cusco"> 
-      <div class="carousel-caption d-none d-md-block">
+      <div class="turismo">
         <h5>Cuzco es una ciudad de los Andes peruanos que fue la capital del Imperio Inca y es conocida por sus restos arqueológicos <br>
          y la arquitectura colonial española. <br>
          La Plaza de Armas es el centro de la ciudad antigua, con galerías, balcones de madera tallada y ruinas de murallas incas. 
@@ -44,10 +57,10 @@ echo " City Tours"
         
       </div>
     </div>
-    <div class="lugares turísticos de las diferentes regiones del Perú.">
+    <div class="zona">
     <h5>LAS ZONAS TURISTICAS EN LIMA  </h5>
       <img src="https://travel1tours.com/wp-content/uploads/2020/07/lugares-turisticos-lima.jpg" class="d-block w-100"  alt="lima">
-      <div class="carousel-caption d-none d-md-block">
+      <div class="turismo ">
         <h5>Lima es la capital de Perú ubicada en la árida costa del Pacífico del país.
              <br> Pese a que su centro colonial se conserva, es una desbordante metrópolis y una de las ciudades más grandes de Sudamérica. 
              <br> El Museo Larco alberga una colección de arte precolombino <br>
@@ -56,10 +69,10 @@ echo " City Tours"
               <a href="https://www.denomades.com/blog/lugares-turisticos-lima-debes-conocer/" class="underline">SIGUIENTE AQUI</a> <br> GRACIAS.
       </div>
     </div>
-    <div class="lugares turísticos de las diferentes regiones del Perú.">
+    <div class="zona">
     <h5>LAS ZONAS  TURISTICA DE TINGO MARIA</h5>
       <img src="https://www.tingomarianunashtours.com.pe/assets/webimg/2021-10-29_OIOICQIG.jpg" class="d-block w-100" alt="zona turistica de tingo maria">
-      <div class="carousel-caption d-none d-md-block">
+      <div class="turismo">
         <h5>Tingo María es una ciudad en el río Huallaga, en el centro de Perú, entre las montañas andinas y la selva amazónica. 
         <br> Aquí se encuentra el Parque nacional Tingo María que posee una abundante biodiversidad y <br>
          es conocido por la Bella Durmiente, una montaña con la forma de una mujer acostada. 
@@ -70,10 +83,10 @@ echo " City Tours"
       </div>
     </div>
   </div>
-  <div class="lugares turísticos de las diferentes regiones del Perú.">
+  <div class="zona">
     <h5>LAS ZONAS  TURISTICA DE ICA</h5>
       <img src="https://sites.google.com/site/regionicajulia/_/rsrc/1413838763903/home/Imagen1.jpg" class="d-block w-100" alt="zona turistica de ica ">
-      <div class="carousel-caption d-none d-md-block">
+      <div class="turismo">
         <h5>Ica es una ciudad del centro sur del Perú, capital del departamento de Ica, <br>
          situada en el estrecho valle que forma el río Ica, entre el Gran Tablazo de Ica y las <br>
           laderas occidentales de la cordillera de los Andes. <br>
@@ -82,10 +95,10 @@ echo " City Tours"
       </div>
     </div>
   </div>
-  <div class="lugares turísticos de las diferentes regiones del Perú.">
+  <div class="zona">
     <h5>LAS ZONAS  TURISTICA DE PASCO</h5>
       <img src="https://www.inforegion.pe/wp-content/uploads/2013/07/MAPA-DEL-CORREDOR-TURISTICO-QUEBRADA-CHAUPIHUARANGA.jpg" class="d-block w-100" alt="zona turistica de Pasco ">
-      <div class="carousel-caption d-none d-md-block">
+      <div class="turismo">
         <h5>Pasco es un departamento de la República del Perú ubicado en el centro del país, con capital en Cerro de Pasco. <br>
          Comprende una región andina en su parte oeste y una amazónica en la oriental. Limitando al norte con Huánuco, <br>
          al este con Ucayali, al sur con Junín y al oeste con Lima.</h5>
@@ -93,26 +106,46 @@ echo " City Tours"
       </div>
     </div>
   </div>
-  <div class="lugares turísticos de las diferentes regiones del Perú.">
+  <div class="zona">
     <h5>LAS ZONAS  TURISTICA DE HUANUCO</h5>
       <img src="https://www.somosperu.org.pe/wp-content/uploads/2016/10/Huanuco-turismo.jpg" class="d-block w-100" alt="zona turistica de huánuco ">
-      <div class="carousel-caption d-none d-md-block">
+      <div class="turismo">
         <h5>Huánuco es una ciudad peruana, capital del distrito, la provincia <br>
          y el departamento homónimos en el centro norte del país.</h5>
          <a href="https://www.viajesfantasticos.net/lugares-turisticos-de-huanuco/" class="underline">SIGUIENTE AQUI</a> <br> GRACIAS.
       </div>
     </div>
   </div>
-  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
-    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-    <span class="visually-hidden">Previous</span>
-  </button>
-  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
-    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-    <span class="visually-hidden">Next</span>
-    
-  </button>
+  <div class="zona">
+    <h5>LAS ZONAS  TURISTICA DE PUNO</h5>
+      <img src="https://travel1tours.com/wp-content/uploads/2020/01/Lugares-turisticos-de-Puno.jpg" class="d-block w-100" alt="zona turistica de puno ">
+      <div class="turismo">
+        <h5>En Puno podrás ser parte de una atmósfera mágica donde la leyenda, las tradiciones y las fiestas multicolor se respiran todos los dí­as. <br>
+         Sus sorprendentes islas flotantes como los Uros, fabricadas con esteras de totoras, solo son superadas por el mí­stico Lago Titicaca. <br>
+          Un pueblo orgulloso de su pasado de tradición folclórica que se desborda cada año en danzas y ritos llega a su máxima expresión <br>
+           en la gran fiesta en honor a la Virgen de la Candelaria. <br>
+         No hay motivo para que no disfrutes de su riqueza</h5>
+         <a href="https://www.viajesfantasticos.net/lugares-turisticos-de-puno/" class="underline">SIGUIENTE AQUI</a> <br> GRACIAS.
+      </div>
+    </div>
+  </div>
+  <div class="zona">
+    <h5>LAS ZONAS  TURISTICA EN AMAZONAS</h5>
+      <img src="https://www.tomplanmytrip.com/wp-content/uploads/2021/03/Turismo-amazonas-colombiano.jpg" class="d-block w-100" alt="zona turistica en amazonas ">
+      <div class="turismo">
+        <h5>Amazonas es una región hacia el norte de Perú en la frontera con Ecuador. <br>
+         Es conocida por sus densos bosques nubosos, la escarpada cordillera de los Andes y unas ruinas antiguas. <br>
+         El sitio arqueológico más destacado es Kuélap, un asentamiento preincaico que tiene cientos de construcciones y una gran muralla perimetral. <br>
+         Hacia el sur se encuentra Chachapoyas, la capital de la región, la que presenta techos de tejas rojas</h5>
+         <a href="https://delamazonas.com/peru/lugares-turisticos/" class="underline">SIGUIENTE AQUI</a> <br> GRACIAS.
+      </div>
+    </div>
+  </div>
   
 
-</div>
+ 
+  </div>
 </BODY>
+
+
+  
